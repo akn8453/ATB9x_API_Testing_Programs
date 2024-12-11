e9e9d3cdef715efca0b147c1b9e8e4612266d57d
Akshay <akn8453@gmail.com>
2024-12-11 10:48:50 +0530

    Initial commit

---
+++ src/main/java/com/akshaynomulwar/App.java
@@ -0,0 +1,13 @@
+package com.akshaynomulwar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
